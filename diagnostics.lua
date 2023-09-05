efdc898b5d6c3e5c313b7c4522558dfc9d4c9cae
rogersanctus <rogersanctus@gmail.com>
2023-09-05 05:37:08 -0300

    Adjusts the diagnostics options

--- diagnostics.lua
+++ diagnostics.lua
@@ -1,5 +1,5 @@
 return {
-  virtual_text = true,
+  virtual_text = false, -- Disabling virtual text to avoid offuscation of Codeium suggestions.
   underline = true,
-  update_in_insert = false,
+  update_in_insert = true,
 }
