46152aa35af744af37f15180e63e25b6156e498a
rogersanctus <rogersanctus@gmail.com>
2023-09-05 05:39:35 -0300

    Set termguicolors on vim.opt and adjusts guicursor option

--- options.lua
+++ options.lua
@@ -7,14 +7,13 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
     swapfile = false,
-    guifont = "ProFont IIx Nerd Font:h14",
-    guicursor = 'n-v-c:block-Cursor,i-ci-ve:ver25-Cursor-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,r-cr:hor20-Cursor,o:hor50'
+    termguicolors = true,
+    guicursor = 'n-v-c:block-Cursor,i-ci-ve:ver25-iCursor-blinkwait500-blinkoff400-blinkon200,r-cr:hor20-Cursor,o:hor50'
   },
   g = {
     autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
     cmp_enabled = true, -- enable completion at start
     autopairs_enabled = true, -- enable autopairs at start
-    --diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
   },
