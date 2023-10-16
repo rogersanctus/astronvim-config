40bc765b4149518a5559d3093a244cccd4822c41
rogersanctus <rogersanctus@gmail.com>
2023-10-16 00:16:27 -0300

    Fixes a formatting issue with elixir .heex files

--- lsp/formatting.lua
+++ lsp/formatting.lua
@@ -9,4 +9,10 @@ return {
       "typescriptreact",
     },
   },
+  filter = function(client)
+    -- Disables html builtin formatting on save for heex files as it conflicts with elixir formatter
+    if client.name == "html" then return vim.bo.filetype ~= "heex" end
+    -- And enables elixir formatter
+    if client.name == "elixirls" then return true end
+  end,
 }
