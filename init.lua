dc3ebb74daf8335d236691e907d5496005521113
Rogério Santos <rogersanctus@gmail.com>
2023-11-28 22:35:31 -0300

    Change integrated shell to pwsh when on Windows; Adds a default keymap for goto definition;

--- init.lua
+++ init.lua
@@ -17,8 +17,23 @@ return {
       cache_enabled = 0,
     }
 
+    if jit.os == "Windows" then
+      vim.cmd [[
+        let &shell = executable("pwsh") ? 'pwsh' : 'powershell'
+		    let &shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';"
+	      let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+        let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+		    set shellquote= shellxquote=
+      ]]
+    end
+
     vim.keymap.set("i", "<M-S>", function() return vim.fn["codeium#Complete"]() end, { expr = true })
-    vim.keymap.set("n", "<C-m>", "<C-v>")
+    vim.keymap.set(
+      "n",
+      "gd",
+      "<cmd>normal! gd<CR>",
+      { noremap = true, silent = true, desc = "Go to local declaration" }
+    )
 
     if vim.fn.executable "rg" == 1 then
       vim.opt.grepprg = "rg --vimgrep --smart-case --hidden"
