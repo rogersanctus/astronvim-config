e51e4734c99a42c641d0c063a5ee5f55a90de64e
rogersanctus <rogersanctus@gmail.com>
2024-01-13 18:33:09 -0300

    Configures npm-groovy-lint as a Conform formatter

--- plugins/formatting.lua
+++ plugins/formatting.lua
@@ -2,12 +2,23 @@ return {
   "stevearc/conform.nvim",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    require("conform").setup {
+    local conform = require "conform"
+
+    conform.formatters.groovyfmt = function(bufn)
+      return {
+        stdin = true,
+        command = "npm-groovy-lint",
+        args = { "--format", "-" },
+      }
+    end
+
+    conform.setup {
       formatters_by_ft = {
         lua = { "stylua" },
+        groovy = { "groovyfmt" },
       },
       format_on_save = {
-        timeout_ms = 500,
+        timeout_ms = 800,
         lsp_fallback = true,
         async = false,
       },
