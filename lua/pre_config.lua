021a33a8000c86ff299db61485f85adcd4a87f6a
Rogério Ferreira <rogersanctus@gmail.com>
2025-08-09 16:27:33 -0300

    Fixes the vim.uri_to_bufnr function

--- lua/pre_config.lua
+++ lua/pre_config.lua
@@ -3,4 +3,30 @@ vim.filetype.add {
     hx = "haxe",
     hxml = "hxml",
   },
+  pattern = {
+    [".*%.blade%.php"] = "blade",
+  },
 }
+
+-- Return the buffer number for a given filename or -1 if it doesn't exist in the buffer list
+local function buf_exists_with_name(name)
+  local bufs = vim.api.nvim_list_bufs()
+
+  for _, buf in ipairs(bufs) do
+    local buf_name = vim.api.nvim_buf_get_name(buf)
+
+    if buf_name == name then return buf end
+  end
+
+  return -1
+end
+
+-- Overrides vim.uri_to_bufnr to fix a bug where it can't find the buffer number
+vim.uri_to_bufnr = function(uri)
+  local fname = vim.uri_to_fname(uri)
+  local bufnr = buf_exists_with_name(fname)
+
+  if bufnr == -1 then return vim.fn.bufadd(fname) end
+
+  return bufnr
+end
