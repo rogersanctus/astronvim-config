76c9313e652a22850a5449a0c01edec81ff7d3dd
Rogério Ferreira <rogersanctus@gmail.com>
2025-10-01 01:45:30 -0300

    feat: make some adjustments to make clang and lemminx work on aarch64 architecture - and they must be compiled and installed manually

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,14 +1,17 @@
+local lemminx_path =
+  vim.fn.expand "$HOME/sources/lemminx/org.eclipse.lemminx/target/lemminx-linux-aarch_64-0.31.1-SNAPSHOT"
+
 -- use mason-lspconfig to configure LSP installations
 return {
   "williamboman/mason-lspconfig.nvim",
-  opts = {
-    automatic_installation = {
-      exclude = {
-        "vtsls",
-      },
-    },
-    ensure_installed = {
-      "clangd",
+  opts = function(_, opts)
+    local arch = vim.uv.os_uname().machine
+
+    local exclude_tbl = {
+      "vtsls",
+    }
+
+    local ensure_installed = {
       "cssls",
       "eslint",
       "html",
@@ -24,6 +27,31 @@ return {
       "tailwindcss",
       "emmet_language_server",
       "zls",
-    },
-  },
+    }
+
+    if arch == "aarch64" then
+      table.insert(exclude_tbl, "lemminx")
+      table.insert(exclude_tbl, "clangd")
+
+      -- Lsp Config
+      local lspconfig = require "lspconfig"
+
+      lspconfig.lemminx.setup {
+        cmd = { lemminx_path },
+      }
+
+      lspconfig.clangd.setup {
+        cmd = { "clangd" },
+      }
+    else
+      table.insert(ensure_installed, "clangd")
+    end
+
+    return vim.tbl_deep_extend("force", opts, {
+      automatic_installation = {
+        exclude = exclude_tbl,
+      },
+      ensure_installed = ensure_installed,
+    })
+  end,
 }
